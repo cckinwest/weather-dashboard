beea5f7108e2e0de00485eb4dcc0034f8f9630be
cckinwest <kensechan@gmail.com>
2023-05-10 00:07:36 +0100

    add trash button, add errorhandling, and readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # weather-dashboard
+
+I use the 5 Day Weather Forecast API to to retrieve weather data for cities and present it as a dashboard.
+
+## Functionalities
+
+When I search for a city, then I am presented with current and future conditions for that city and that city is added to the search history; when I view current weather conditions for that city, then I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed; when I view future weather conditions for that city, then I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity; when I click on a city in the search history, then I am again presented with current and future conditions for that city.
+
+## Features
+
+The dashboard consists of a left panel and a right panel. The left panel shows the input field, the search button and the history buttons, the right hand panel shows the current weather and weather forecasts.
+
+### Left Panel
+
+When the page is loaded, historical data will be drawn from localStorage and history buttons will be rendered, so that when you click on these buttons directly, the corresponding weather data will be shown on the right.
+
+You first enter the name of a city in the input field. If data can be found for that name, the data will be displayed on the right and a history button will be created for that name if it's a new search; otherwise, a warning message will be shown on the right and no history button is created. Besides each history button, a trash button is used to delete that button, and the record about that city will also be deleted as well.
+
+### Right Panel
+
+When the page is loaded, a message on this panel is shown to instruct how to use the dashboard.
+
+Data is drawn from APIs. The top part of the right panel shows the current data, and the bottom part shows the weather forecast. When there is a new search of city, the right panel will be refreshed and the new weather data is presented. When an invalid search is made, a warning message will be shown on the right panel.
+
+!["Start of the dashboard"](./image/start.png "Start of the dashboard")
+
+!["Display of the dashboard"](./image/display.png "Display of the dashboard")
+
+!["Error handling of the dashboard"](./image/error.png "Error handling of the dashboard")
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const APIKEY = "b6f0ccf06b4649cf66be86ca82eec322";
 
-//const APIKEY = config.APIKEY;
 const forecastAPI = "https://api.openweathermap.org/data/2.5/forecast";
 const geocodingAPI = "https://api.openweathermap.org/geo/1.0/direct";
+const currentAPI = "https://api.openweathermap.org/data/2.5/weather";
 
 var header = $("header");
 header.addClass(
@@ -10,6 +10,7 @@ header.addClass(
 );
 
 var searchPanel = $("#searchPanel");
+var historyArea = $("#historyArea");
 var displayPanel = $("#displayPanel");
 var currentPanel = $("#currentWeather");
 var forecastPanel = $("#forecastWeather");
@@ -17,29 +18,32 @@ var forecastPanel = $("#forecastWeather");
 var cityField = $(".cityField");
 var searchBtn = $(".searchBtn");
 
-function forecast(lat, lon) {
-  currentPanel.empty();
-  forecastPanel.empty();
-
-  var url = `${forecastAPI}?lat=${lat}&lon=${lon}&units=metric&appid=${APIKEY}`;
+function current(lat, lon) {
+  var url = `${currentAPI}?lat=${lat}&lon=${lon}&units=metric&appid=${APIKEY}`;
 
   fetch(url)
     .then(function (response) {
-      return response.json();
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        errorHandling();
+      }
     })
     .then(function (data) {
-      var city = cityField.val();
+      if (data.length !== 0) {
+        $("<h2></h2>").text("Current weather:").appendTo(currentPanel);
 
-      for (var i = 0; i < data.list.length; i += 8) {
-        var temp = data.list[i].main.temp;
-        var wind = data.list[i].wind.speed;
-        var humidity = data.list[i].main.humidity;
-        var weatherIcon = data.list[i].weather[0].icon;
-        var date = dayjs(data.list[i].dt * 1000).format("YYYY/MM/DD h A");
+        var city = cityField.val();
+
+        var temp = data.main.temp;
+        var wind = data.wind.speed;
+        var humidity = data.main.humidity;
+        var weatherIcon = data.weather[0].icon;
+        var date = dayjs(data.dt * 1000).format("YYYY/MM/DD h A");
 
         var infoPanel = $("<div></div>");
 
-        infoPanel.addClass("bg-secondary border rounded-3 p-3");
+        infoPanel.addClass("bg-info border rounded-3 p-3");
 
         var cityTitle = $("<p></p>")
           .text(`${city} (${date})`)
@@ -62,26 +66,130 @@ function forecast(lat, lon) {
           .text(`Humidity: ${humidity}%`)
           .appendTo(infoPanel);
 
-        if (i === 0) {
-          infoPanel.appendTo(currentPanel);
-        } else {
-          infoPanel.addClass("col col-3");
-          infoPanel.appendTo(forecastPanel);
-        }
+        infoPanel.addClass("col m-auto");
+        infoPanel.appendTo(currentPanel);
+      } else {
+        errorHandling();
       }
     });
+}
+
+function forecast(lat, lon) {
+  var url = `${forecastAPI}?lat=${lat}&lon=${lon}&units=metric&appid=${APIKEY}`;
+
+  fetch(url)
+    .then(function (response) {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        errorHandling();
+      }
+    })
+    .then(function (data) {
+      if (data.length !== 0) {
+        $("<h2></h2>").text("Forecast:").appendTo(forecastPanel);
+
+        var city = cityField.val();
+
+        for (var i = 7; i < data.list.length; i += 8) {
+          var temp = data.list[i].main.temp;
+          var wind = data.list[i].wind.speed;
+          var humidity = data.list[i].main.humidity;
+          var weatherIcon = data.list[i].weather[0].icon;
+          var date = dayjs(data.list[i].dt * 1000).format("YYYY/MM/DD h A");
+
+          var infoPanel = $("<div></div>");
+
+          infoPanel.addClass("col bg-info border rounded-3 p-3 m-auto");
+
+          var cityTitle = $("<p></p>")
+            .text(`${city} (${date})`)
+            .appendTo(infoPanel);
+
+          var weatherImg = $("<img></img>");
+          weatherImg.attr(
+            "src",
+            `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+          );
+          weatherImg.appendTo(infoPanel);
+
+          var tempText = $("<p></p>")
+            .text(`Temp: ${temp} \u00B0C`)
+            .appendTo(infoPanel);
+          var windText = $("<p></p>")
+            .text(`Wind: ${wind} MPH`)
+            .appendTo(infoPanel);
+          var humidText = $("<p></p>")
+            .text(`Humidity: ${humidity}%`)
+            .appendTo(infoPanel);
+
+          infoPanel.appendTo(forecastPanel);
+        }
+      } else {
+        errorHandling();
+      }
+    });
+}
+
+function errorHandling() {
+  currentPanel.empty();
+  forecastPanel.empty();
+
+  console.log("Something goes wrong. Check if you enter correctly.");
+
+  $("<h1></h1>")
+    .text("Something goes wrong. Check if you enter correctly.")
+    .addClass("display-1")
+    .appendTo(currentPanel);
 }
 
 function createBtn(city) {
   var historyBtn = $("<button></button>");
   historyBtn.text(city);
-  historyBtn.addClass("btn btn-success btn-lg w-100 mb-2 historyBtn");
-  historyBtn.appendTo(searchPanel);
+  historyBtn.addClass("btn btn-success btn-lg w-75 mb-2 historyBtn");
+  historyBtn.attr("id", city);
+  historyBtn.appendTo(historyArea);
 
   historyBtn.on("click", function () {
     cityField.val(city);
     searchWeather(city);
   });
+
+  var clearBtn = $("<button></button>");
+  clearBtn.addClass("btn btn-danger btn-lg w-25 mb-2 text-wrap");
+  clearBtn.attr("id", city);
+  clearBtn.appendTo(historyArea);
+
+  clearBtn.on("click", clear);
+
+  var clearText = $("<i></i>", {
+    class: "fas fa-trash-alt",
+    ariaHidden: "true",
+  }).appendTo(clearBtn);
+}
+
+function clear() {
+  var city = $(this).attr("id");
+  var history = JSON.parse(localStorage.getItem("history"));
+
+  var ind = 0;
+  for (var i = 0; i < history.length; i++) {
+    if (history[i] === city) {
+      ind = i;
+    }
+  }
+
+  var before = history.slice(0, ind);
+  var after = history.slice(ind + 1);
+  history = before.concat(after);
+
+  localStorage.setItem("history", JSON.stringify(history));
+
+  historyArea.empty();
+
+  for (var i = 0; i < history.length; i++) {
+    createBtn(history[i]);
+  }
 }
 
 function searchWeather(city) {
@@ -89,13 +197,29 @@ function searchWeather(city) {
 
   fetch(url)
     .then(function (response) {
-      return response.json();
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        errorHandling();
+        return false;
+      }
     })
     .then(function (data) {
-      var lat = data[0].lat;
-      var lon = data[0].lon;
+      if (data.length !== 0) {
+        var lat = data[0].lat;
+        var lon = data[0].lon;
 
-      forecast(lat, lon);
+        currentPanel.empty();
+        forecastPanel.empty();
+
+        current(lat, lon);
+        forecast(lat, lon);
+
+        return true;
+      } else {
+        errorHandling();
+        return false;
+      }
     });
 }
 
@@ -107,9 +231,7 @@ searchBtn.on("click", () => {
     history = JSON.parse(localStorage.getItem("history"));
   }
 
-  searchWeather(city);
-
-  if (!history.includes(city)) {
+  if (!history.includes(city) && searchWeather(city)) {
     createBtn(city);
     history.push(city);
     localStorage.setItem("history", JSON.stringify(history));
@@ -126,4 +248,9 @@ $(function () {
   for (var i = 0; i < history.length; i++) {
     createBtn(history[i]);
   }
+
+  $("<h1></h1>")
+    .text("Enter a city and search. The weather will be shown here.")
+    .addClass("display-1")
+    .appendTo(currentPanel);
 });
